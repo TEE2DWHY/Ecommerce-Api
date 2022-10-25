2b38c77eb3f5868ba99b50f929f7780a44759d5b
TEE2DWHY <88804937+TEE2DWHY@users.noreply.github.com>
2022-10-25 18:17:35 +0100

    added changes on the product route

--- routes/product.js
+++ routes/product.js
@@ -1,29 +1,55 @@
 // We would be using the express router: it is used to handle request
 const router = require("express").Router();
 const Product = require("../models/Product");
-const User = require("../models/User");
-// const verifyTokenAndAdmin = require("./verifyToken")
+const { verifyTokenAndAdmin } = require("./verifyToken")
 
 
 
-// // REGISTER
+// ADD PRODUCT
+router.post("/", verifyTokenAndAdmin, async (req, res) => {
+    const newProduct = new Product(req.body)
+    try {
+        const savedProduct = await newProduct.save();
+        res.status(200).json(savedProduct);
+    }
+    catch (err) {
+        res.status(500).json(err)
+    }
 
+});
 
+// UPDATE
+router.put("/:title", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const updatedProduct = await Product.findOneAndUpdate(req.params.title,
+            { $set: req.body }, { new: true });
 
+        res.status(200).json(updatedProduct)
+    } catch (err) {
+        res.status(500).json(err);
+    }
 
+});
 
-// //UPDATE
-// router.put("/:product", verifyTokenAndAdmin, async (req, res) => {
-//     if (User.isAdmin === true) {
-//         try {
-//             const updatedProduct = await Product.findByIdAndUpdate(req.body.price, req.body.size,
-//                 { $set: req.body }, { new: true });
+//DELETE
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        await Product.findByIdAndDelete(req.params.id);
+        res.status(200).json("successfully deleted")
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
-//             res.status(200).json(updatedProduct)
-//         } catch (err) {
-//             res.status(500).json(err);
-//         }
-//     }
-// })
+//GET ALL PRODUCTS
+router.get("/", async (req, res) => {
+    try {
+        const allProducts = await Product.find();
+
+        res.status(200).json(allProducts);
+    } catch (err) {
+        res.status(500), json(err);
+    }
+});
 
 module.exports = router
